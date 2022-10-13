6416a4d906b3dccf9faab065e752aaadd12026e6
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-10-13 16:14:10 +0700

    Yarp

---
+++ src/Practical.ReverseProxy/Practical.ReverseProxy.Yarp/Program.cs
@@ -0,0 +1,21 @@
+namespace Practical.ReverseProxy.Yarp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add the reverse proxy to capability to the server
+            var proxyBuilder = builder.Services.AddReverseProxy();
+
+            // Initialize the reverse proxy from the "ReverseProxy" section of configuration
+            proxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+            var app = builder.Build();
+
+            app.MapReverseProxy();
+            app.Run();
+        }
+    }
+}
